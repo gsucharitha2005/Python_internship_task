ef6fcc2c39afcac02a5832276fa4c234ad4af2dc
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-21 14:49:25 +0530

    students score comparision

---
+++ task2.py
@@ -0,0 +1,12 @@
+student1=50
+student2=49
+student3=55
+student4=60
+student5=75
+print(student1==student2) 
+print(student4!=student1)
+print(student4>student5)
+print(student3<student4)
+print(student1>=student3)
+print(student3<=student2)
+
