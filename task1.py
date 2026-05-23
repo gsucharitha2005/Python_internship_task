db04226cda37d8691c269b7862cd0e33ec7f07db
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-23 21:59:38 +0530

    update

--- task1.py
+++ task1.py
@@ -1,4 +1,10 @@
-library={"books":["python", "java", "C"]}
-print("sql" in library["books"])
-print("java" in library["books"])
-print("HTML" not in library["books"])
+user1="sucharitha"
+password1=1234567
+user2="alakambhavya"
+password2=98765
+username=input("enter username:")
+password=input("enter password:")
+login1=("sucharitha"==user1 ) and (1234567==password1)
+login2=("alakambhavya"==user2) and (password2==98765)
+
+print("login successful:",(login1 is False) or(login2 is False) )
