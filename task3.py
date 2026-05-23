e12070e4bc4c02aec0eb274aeb1a1101a713007c
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-23 19:38:55 +0530

    encode,decode message using bitwise XOR operations

---
+++ task3.py
@@ -0,0 +1,6 @@
+
+data=[10,20,30,40,50]
+key=2
+print("encode:",data[0]^key,data[1]^key,data[2]^key,data[3]^key,data[4]^key)
+print("decode:",(data[0]^key)^key,(data[1]^key)^key,(data[2]^key)^key,(data[3]^key)^key,(data[4]^key)^key)
+                                  
