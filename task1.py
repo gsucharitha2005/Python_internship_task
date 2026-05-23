ffd918ba1e7c516bff001ef2bcf6fb3333127268
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-23 22:12:48 +0530

    library management system

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-data=[10,20,30,40,50]
-key=2
-print("encode:",data[0]^key,data[1]^key,data[2]^key,data[3]^key,data[4]^key)
-print("decode:",(data[0]^key)^key,(data[1]^key)^key,(data[2]^key)^key,(data[3]^key)^key,(data[4]^key)^key)
+library={"books":["python", "java", "C"]}
+print("sql" in library["books"])
+print("java" in library["books"])
+print("HTML" not in library["books"])
